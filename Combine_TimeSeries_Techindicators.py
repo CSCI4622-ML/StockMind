0a1c57c513523c7ba964bc92527af3d7fb74e861
PeterMcD1 <pemc6694@colorado.edu>
2023-04-02 23:55:00 -0600

    combine timeseries works if called from a script

--- Combine_TimeSeries_Techindicators.py
+++ Combine_TimeSeries_Techindicators.py
@@ -1,16 +1,18 @@
 import os
 import pandas as pd
 
-files = os.listdir('market_data/TechIndicators')
-os.makedirs('market_data/merged_data', exist_ok=True)
+script_path = os.path.dirname(os.path.abspath(__file__))
+
+files = os.listdir(f'{script_path}/market_data/TechIndicators')
+os.makedirs(f'{script_path}/market_data/merged_data', exist_ok=True)
 
 for filename in files:
     company = os.path.splitext(filename)[0]
 
-    techindicators_path = os.path.join('market_data/TechIndicators', f'{company}.csv')
-    timeseries_path = os.path.join('market_data/TimeSeries', f'{company}.csv')
+    techindicators_path = os.path.join(f'{script_path}/market_data/TechIndicators', f'{company}.csv')
+    timeseries_path = os.path.join(f'{script_path}/market_data/TimeSeries', f'{company}.csv')
     techindicators_df = pd.read_csv(techindicators_path)
     timeseries_df = pd.read_csv(timeseries_path)
     merged_df = pd.merge(techindicators_df, timeseries_df, on='date')
-    output_path = os.path.join('market_data/merged_data', f'{company}.csv')
+    output_path = os.path.join(f'{script_path}/market_data/merged_data', f'{company}.csv')
     merged_df.to_csv(output_path, index=False)
