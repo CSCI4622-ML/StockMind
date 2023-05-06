ca268536fd54991863cedb2c88fc5159f9806b8a
connordavel <coda3831@colorado.edu>
2023-05-06 03:16:19 -0600

    Added additional tickers for data collection

--- market_data/setup.py
+++ market_data/setup.py
@@ -8,6 +8,8 @@ from alpha_vantage_key import *
 import time
 import json
 import csv
+import requests
+from datetime import datetime, timedelta
 
 # hande the data setup in parallel file structure will be as below:
 # FILE STRUCTURE
@@ -24,7 +26,7 @@ import csv
 #       <symbol2>.csv
 
 def output_query(data, file_location, replace_existing=False):
-    file = Path(file_location)
+    file = Path(".") / Path(file_location)
     if file.exists() and file.is_file():
         if replace_existing:
             print(f"replacing {file_location} with updated data")
@@ -36,10 +38,10 @@ def output_query(data, file_location, replace_existing=False):
         file.parent.mkdir(parents=True, exist_ok=True)
 
     if isinstance(data, pd.DataFrame):
-        data.to_csv(file, sep=',', header=True)
+        data.to_csv(str(file.absolute()), sep=',', header=True)
     elif isinstance(data, dict):
         json_object = json.dumps(data, indent=4)
-        with open(file_location, "w") as outfile:
+        with open(str(file.absolute), "w") as outfile:
             outfile.write(json_object)
     # else:
     #     with open(file_location, "w") as outfile:
@@ -59,12 +61,12 @@ def query_limit():
         time.sleep(ALPHA_VANTAGE_QUERY_LIMIT - time_since_last_query)
 
 
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname) # change location to setup file's directory
+# abspath = os.path.abspath(__file__)
+# dname = os.path.dirname(abspath)
+# os.chdir(dname) # change location to setup file's directory
 
 # symbols that will be used when querying all data
-symbols = ['AAPL', 'MSFT', 'GOOG', 'GOOGL', 'AMZN', 'PCAR', 'TSLA', 'NVDA', 'V', 'TSM', 'UNH']
+symbols = ['AAPL', 'MSFT', 'TSLA', 'META', 'XOM', 'NVDA', 'AMZN', 'JPM', 'GOOG', 'SHOP', 'AMD', 'AFRM', 'BAC', 'ADBE', 'SQ', 'AVGO', 'BKNG', 'DKNG', 'TEAM', 'RDFN', 'COIN', 'OPEN', 'PLUG', 'WMT', 'XOM', 'MA', 'KO', 'UNH', 'PG', 'BIO']
 
 #------------------TIME SERIES DATA-----------------------
 output_dir = "TimeSeries"
@@ -146,45 +148,51 @@ for sym in symbols:
 
 #------------------SENTIMENT ANALYSIS  DATA-----------------------
 
-output_dir = "AlphaIntelligence"
-symbols = ["AAPL"] # Limited to reduce testing time
 
-num_articles = 30
-num_months = 15
-starting_month = 2 # Feb
-day_of_month = "01"
+# output_dir = "AlphaIntelligence"
 
-for sym in symbols:
-    output_file = Path(output_dir) / f"{sym}.csv"
-    ts = alpha_vantage.AlphaIntelligence(key=ALPHA_VANTAGE_KEY, output_format='pandas', indexing_type='integer')
-    data, meta_data = None, None
-    print(sym)
+# start_date = datetime(2022, 3, 1, 0, 0, 0)
+# end_date = datetime(2023, 5, 4, 0, 0, 0)
+# day_count = (end_date - start_date).days
+# day_query_range = 1
+# query_days = range(0, day_count, day_query_range)
+# for sym in symbols:
+#     output_file = Path(output_dir) / f"{sym}.csv"
+#     # key=ALPHA_VANTAGE_KEY
+#     dframe = pd.DataFrame(columns=["url", "title", "summary"])
+#     for query_day in (start_date + timedelta(n) for n in query_days):
+#         print(query_day)
+#         query_next_day = query_day + timedelta(day_query_range)
+#         year = query_day.year
+#         month = query_day.month
+#         day = query_day.day
+#         start_date_str = f"{year}{month:02}{day:02}T0000"
 
-    for i in range(num_months):
-        start_year = "{}".format(2022 + (i + starting_month - 1) // 12)
-        start_month = "{:02d}".format((i + starting_month - 1) % 12 + 1)
+#         n_year = query_next_day.year
+#         n_month = query_next_day.month
+#         n_day = query_next_day.day
+#         end_date_str = f"{n_year}{n_month:02}{n_day:02}T0000"
+#         url = rf"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={sym}&time_from={start_date_str}&time_to={end_date_str}&limit=10&sort=RELEVANCE&apikey={ALPHA_VANTAGE_KEY}"
+#         query_limit()
+#         while True:
+#             try:
+#                 r = requests.get(url)
+#                 data = r.json()
+#                 break
+#             except Exception as e:
+#                 print(e)
+#                 time.sleep(10)
 
-        end_year = "{}".format(2022 + (i + starting_month) // 12)
-        end_month = "{:02d}".format((i  + starting_month) % 12 + 1)
+#         if len(data) == 1:
+#             print(f"No news data for {query_day}")
+#             continue
+#         for news_article in data["feed"]:
+#             data_entry = {}
+#             time_published = datetime.strptime(news_article["time_published"], '%Y%m%dT%H%M%S')
+#             data_entry["title"] = news_article["title"].replace("\n", " ")
+#             data_entry["summary"] = news_article["summary"].replace("\n", " ")
+#             data_entry["url"] = news_article["url"]
             
-        time_from = start_year + start_month + day_of_month + "T0000"
-        time_to = end_year + end_month + day_of_month + "T0000"
-
-        print(i, "=> From:", time_from, "- To:", time_to)
-
-        query_limit()
-        try:
-            cur_data, cur_meta_data = ts.get_news(symbol=sym, time_from=time_from, time_to=time_to, limit=num_articles, sort="RELEVANCE")
-
-            cur_data['index'] = [ind + num_articles*i for ind in cur_data['index']]
-            cur_data.index = [ind + num_articles*i for ind in cur_data.index]
-
-            if (data is None):
-                data = cur_data
-                meta_data = cur_meta_data
-            else:
-                data = pd.concat([data, cur_data])
-        except:
-            print("No articles found")
-    
-    output_query(data, output_file, replace_existing=True)
+#             dframe = pd.concat([pd.DataFrame(data_entry, index=[time_published]), dframe], axis=0)
+#     dframe.sort_index(inplace=True)
+#     output_query(dframe, output_file, replace_existing=True)
